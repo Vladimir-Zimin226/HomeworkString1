6ace334755a20773cccff73945ec0f8cfb84cb07
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-10-10 22:44:14 +1100

    Homework Completed!

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        String phone = "+796057-987-98";
-        phone = phone.replace("-", "");
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
-        if (phone.length() == 10) {
-            phone = '7' + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("Телефон слишком длинный!!!");
-        } else if (phone.length() < 10) {
-            throw new RuntimeException("Телефон слишком короткий!!!");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
-            throw new RuntimeException("Среди нас посторонний!!!");
-        }
-        System.out.println("phone = " + phone);
-        String expectedPhone = "79605798798";
-        if (phone.equals(expectedPhone)) {
-            System.out.println("Успех!!!");
-        } else {
-            System.out.println("Неудача(((");
-        }
+       //Task 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+
+        //Task 2
+        String fullNameToApperCase = fullName.toUpperCase();
+        System.out.println(fullNameToApperCase);
+
+        //Task 3
+        String fullNameForTask3 = "Иванов Семён Семёнович";
+        fullNameForTask3 = fullNameForTask3.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameForTask3);
     }
 }
